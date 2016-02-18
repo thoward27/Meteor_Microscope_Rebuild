3b62e9770a77760db881e551f519d1bcf302c9e3
thoward27 <thoward12@outlook.com>
2016-02-17 20:35:36 -0500

    before user integration

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,16 +1,5 @@
-var postsData = [{
-    title: 'Google',
-    url: 'http://www.google.com',
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com',
-  },
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com',
-  },
-];
 Template.postsList.helpers({
-  posts: postsData,
+  posts: function() {
+    return Posts.find();
+  }
 });
