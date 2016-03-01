c68a0a5ca5456f32810c8c35d12d06d924eaf550
thoward27 <thoward12@outlook.com>
2016-03-01 18:01:17 -0500

    Revert "Milestone, working release"
    This reverts commit 91f0fff44cc8d7be95405c9c38284f72f0b90c8b.

--- .meteor/local/build/programs/web.browser/app/client/templates/includes/template.loading.js
+++
@@ -1,8 +0,0 @@
-(function(){
-Template.__checkName("loading");
-Template["loading"] = new Template("Template.loading", (function() {
-  var view = this;
-  return Spacebars.include(view.lookupTemplate("spinner"));
-}));
-
-}).call(this);
--- .meteor/local/build/programs/web.browser/app/client/templates/includes/template.not_found.js
+++
@@ -1,8 +0,0 @@
-(function(){
-Template.__checkName("notFound");
-Template["notFound"] = new Template("Template.notFound", (function() {
-  var view = this;
-  return HTML.Raw('<div class="not-found page jumbotron">\n    <h2></h2>\n    <p>Sorry, we couldn\'t find the page you were looking for :(</p>\n  </div>');
-}));
-
-}).call(this);
